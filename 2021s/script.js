0cb361c1d533e67590b2802a44d532167f5b81de
TamakiFujino <tamaki.fujino@sjsu.edu>
2021-05-02 15:34:08 -0700

    for ipad

--- 2021s/script.js
+++ 2021s/script.js
@@ -28,16 +28,3 @@ $slider.mousewheel(function (e) {
     $(this).slick('slickPrev');
   }
 });
-//////////////// scroll animation
-
-$(function(){
-  $('a[href^=#]').click(function(){
-    var adjust = 0;
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top + adjust;
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
-    return false;
-  });
-});
